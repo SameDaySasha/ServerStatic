5c84ec131255f3507e56747a631205a9cb6fcba8
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-03-02 19:32:25 -0500

    step1Done

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here
+  const fileContent = fs.readFileSync('./index.html')
+
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", 'text/html')
+    
+    res.end(fileContent)
 });
 
-const port = 5000;
+const port = 5001;
 
 server.listen(port, () => console.log('Server is listening on port', port));
