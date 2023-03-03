77944a3e99d96665af1ec50ddbb649ba2d6261da
Jeanette <haltonjeanette@gmail.com>
2023-03-02 16:59:46 -0800

    did the things

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  const fileContent = fs.readFileSync('./index.html')
+  if(req.method === "GET" && req.url.startsWith('/static')){
+    let path = './assets/';
+    path += req.url.slice(8);
+
+    res.statusCode = 200;
+    if(req.url.endsWith('.css')) res.setHeader('Content-Typle', 'text/css');
+    if(req.url.endsWith('.jpg')) res.setHeader('Content-Type', 'image/jpeg');
+    return res.end(fs.readFileSync(path));
+  }
+  const html = fs.readFileSync('./index.html')
+  
 
 
     res.statusCode = 200;
+
     res.setHeader("Content-Type", 'text/html')
     
-    res.end(fileContent)
+    res.end(html);
 });
 
 const port = 5001;
